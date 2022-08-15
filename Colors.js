bccc25b083e018aad25f42d1ee886b5d086384b0
Shawn Kebel <fxrender@outlook.com>
2022-08-14 23:31:46 -0500

    :lipstick: Add more `styles`, add `Colors.js`

---
+++ Colors.js
@@ -0,0 +1,14 @@
+export default colors = {
+  black: "#2d3436",
+  gray: "#a4a4a4",
+  lightGray: "#cacaca",
+  blue: "#24a6d9",
+  lightBlue: "#a7cbd9",
+  red: "#ff0000",
+  white: "#ffffff",
+  green: "#4caf50",
+  brick: "#810010",
+  darkGray: "#515151",
+  darkBlack: "#020202",
+  glass: "rgba(255, 255, 255, 0.2)"
+}
--- styles/AppStyles.js
+++ styles/AppStyles.js
@@ -1,11 +1,27 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  imageContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    padding: 32,
+  },
+
+  noPadding: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "red",
   },
 
   rowContainer: {
@@ -16,12 +32,62 @@ export default StyleSheet.create({
     marginVertical: 4,
   },
 
+  fillSpace: {
+    flex: 1,
+  },
+
+  rightAligned: {
+    justifyContent: "flex-end",
+  },
+
   topMargin: {
     marginTop: 16,
   },
 
+  topMargin1: {
+    marginTop: 8,
+  },
+
+  topMargin3: {
+    marginTop: 32,
+  },
+
+  topMargin0: {
+    marginTop: -32,
+  },
+
   bottomMargin: {
     marginBottom: 16,
+  },
+
+  bottomMargin1: {
+    marginBottom: 32,
+  },
+
+  bottomMargin2: {
+    marginBottom: 48,
+  },
+
+  rightMargin: {
+    marginRight: 16,
+  },
+
+  leftMargin: {
+    marginLeft: 16,
+  },
+
+  margin: {
+    marginRight: 16,
+    marginLeft: 16,
+  },
+
+  margin1: {
+    marginRight: 32,
+    marginLeft: 32,
+  },
+
+  paddingBottom2: {
+    paddingBottom: 32,
   },
 
   backgroundCover: {
@@ -29,12 +95,24 @@ export default StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#000000",
-    opacity: 0.75,
+    opacity: 0.8,
     padding: 16,
   },
 
   lightText: {
     color: "#fff",
+  },
+
+  bluGraText: {
+    color: "#258ea6",
+  },
+
+  gray: {
+    color: "#a4a4a4",
+  },
+
+  darkGray: {
+    color: "#515151",
   },
 
   errorText: {
@@ -49,7 +127,7 @@ export default StyleSheet.create({
     fontSize: 20,
     alignSelf: "stretch",
     padding: 8,
-    borderBottomWidth: 2,
+    borderBottomWidth: 1,
     marginVertical: 8,
   },
 
@@ -58,7 +136,11 @@ export default StyleSheet.create({
   },
 
   darkTextInput: {
-    borderBottomColor: "#000000",
+    borderBottomColor: "#515151",
+  },
+
+  darkTextInput: {
+    borderBottomColor: "#515151",
   },
 
   inlineTextButton: {
