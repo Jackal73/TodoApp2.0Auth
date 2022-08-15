5ab712c37cdf9afb3851de8e088d86dce67333c1
Shawn Kebel <fxrender@outlook.com>
2022-08-14 23:59:06 -0500

    :necktie: Add `tempData` for development purposes

---
+++ tempData.js
@@ -0,0 +1,73 @@
+// Temporary Data until connected with Firebase
+
+export default tempData = [
+  {
+    id: 1,
+    name: "Plan a Trip",
+    color: "rgba(39, 51, 89, 0.4)",
+    todos: [
+      {
+        title: "Book Flights",
+        completed: false,
+      },
+      {
+        title: "Passport Check",
+        completed: true,
+      },
+      {
+        title: "Reserve Hotel Room",
+        completed: true,
+      },
+      {
+        title: "Pack Luggage",
+        completed: false,
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Errands",
+    color: "rgba(209, 89, 219, 0.15)",
+    todos: [
+      {
+        title: "Buy Milk",
+        completed: false,
+      },
+      {
+        title: "Plan Weekend camping trip",
+        completed: true,
+      },
+      {
+        title: "Run 2 miles",
+        completed: true,
+      },
+      {
+        title: "Make a video",
+        completed: true,
+      },
+      {
+        title: "Take dog for a walk",
+        completed: true,
+      },
+    ],
+  },
+  {
+    id: 3,
+    name: "Birthday Party",
+    color: "rgba(0, 255, 0, .15)",
+    todos: [
+      {
+        title: "Get Balloons",
+        completed: false,
+      },
+      {
+        title: "Send Invitations",
+        completed: true,
+      },
+      {
+        title: "Make Dinner Reservations",
+        completed: true,
+      },
+    ],
+  },
+];
