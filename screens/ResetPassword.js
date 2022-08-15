dde3e7a8df1b048f72865521a91422d09270ee60
Shawn Kebel <fxrender@outlook.com>
2022-08-14 23:45:04 -0500

    :passport_control: Add firebase `authentication` `passwordResetEmail`

--- screens/ResetPassword.js
+++ screens/ResetPassword.js
@@ -1,23 +1,35 @@
 import React from "react";
 import { Text, View, TextInput, ImageBackground, Button, KeyboardAvoidingView, Platform } from "react-native";
-
 import InlineTextButton from "../components/InlineTextButton";
 import AppStyles from "../styles/AppStyles";
+import { auth } from "../firebase";
+import { sendPasswordResetEmail } from "firebase/auth";
 
 export default function ResetPassword({ navigation }) {
   const background = require("../assets/background.jpg");
 
   let [email, setEmail] = React.useState("");
-  let [password, setPassword] = React.useState("");
+  let [errorMessage, setErrorMessage] = React.useState("");
+
+  let resetPassword = () => {
+    sendPasswordResetEmail(auth, email)
+      .then(() => {
+        navigation.popToTop();
+      })
+      .catch((error) => {
+        setErrorMessage(error.message);
+      });
+  };
 
   return (
-    <ImageBackground style={AppStyles.container} source={background}>
+    <ImageBackground style={AppStyles.imageContainer} source={background}>
       <KeyboardAvoidingView
         style={AppStyles.backgroundCover}
         behavior={Platform.OS === "ios" ? "padding" : null}
         keyboardVerticalOffset={60}
       >
         <Text style={[AppStyles.lightText, AppStyles.header]}>Reset Password</Text>
+        <Text style={[AppStyles.errorText, AppStyles.topMargin1]}>{errorMessage}</Text>
         <TextInput
           style={[AppStyles.textInput, AppStyles.lightTextInput, AppStyles.lightText]}
           placeholder="Email"
@@ -29,7 +41,7 @@ export default function ResetPassword({ navigation }) {
           <Text style={AppStyles.lightText}>Don't have an account? </Text>
           <InlineTextButton text=" Sign Up" onPress={() => navigation.navigate("SignUp")} />
         </View>
-        <Button title="Reset Password" color="#f7b267" />
+        <Button title="Reset Password" onPress={resetPassword} color="#f7b267" />
       </KeyboardAvoidingView>
     </ImageBackground>
   );
